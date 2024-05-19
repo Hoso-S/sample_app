028a20bd311316037eb7c0d09de3640fe5ef7636
Hoso-S <152981171+Hoso-S@users.noreply.github.com>
2024-05-19 11:07:25 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,17 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # debugger
   end
 
   def new
     @user = User.new
   end
 
-  # POST /users
   def create
-    # @user = User.new(params[:user])    # 実装は終わっていないことに注意!
     @user = User.new(user_params)
     if @user.save
+      reset_session
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -25,6 +24,6 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
+                                   :password_confirmation)
     end
 end
